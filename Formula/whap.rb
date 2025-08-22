a6daf6a926c9f5c881ee82dc9d450127c1db2ef1
GitHub Action <action@github.com>
2025-08-22 04:29:21 +0000

    chore: update whap formula to 0.2.1

--- Formula/whap.rb
+++ Formula/whap.rb
@@ -1,9 +1,9 @@
 class Whap < Formula
   desc "CLI and mock server for testing WhatsApp integration"
   homepage "https://github.com/fdarian/whap"
-  version "0.2.0"
-  url "https://github.com/fdarian/whap/releases/download/v0.2.0/whap-darwin-x64.tar.gz"
-  sha256 "ec4f1139292dd4a171aaf7ddba70c343378c55eb93b3d46cdacc916963f18f65"
+  version "0.2.1"
+  url "https://github.com/fdarian/whap/releases/download/v0.2.1/whap-darwin-x64.tar.gz"
+  sha256 "a2fef8d3cd41099766bc8d8da44f3f49efe516270f3974a7b92d8a63e3c8561f"
   license "MIT"
 
   def install
